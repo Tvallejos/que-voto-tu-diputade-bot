6b8d7a0a90032b66e736bc59f65ad7f3d91afc9c
Tomas <tomas.vallejos12@gmail.com>
2022-05-30 21:58:37 +0200

    undo webhook

--- src/bot.py
+++ src/bot.py
@@ -2,63 +2,70 @@ import os
 import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CallbackContext, CommandHandler
-import sys, os
+import sys
+from dotenv import dotenv_values
 sys.path.append("./src/scrap_diputades/src")
 from full_scrapper import get_votaciones_by_name
-from dotenv import dotenv_values
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
+
 def bulid_vote_msg(vote):
     return str(vote)
 
+
 async def start(update: Update, context: CallbackContext.DEFAULT_TYPE):
     logging.info(f'/start from: {update.effective_chat.id}')
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="I'm a bot, please talk to me!")
 
 
 async def voto(update: Update, context: CallbackContext.DEFAULT_TYPE):
     logging.info(f'/vote from: {update.effective_chat.id}')
     name = update.message.text.split(' ')[1]
     logging.info(f'/vote expecting: {name}, scrapping votos')
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=f'paciencia, {name}')
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f'paciencia, {name}')
     votos = get_votaciones_by_name(name)
     logging.info(f'/vote votos: {votos}')
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=votos)
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text=votos)
     return
 
 if __name__ == '__main__':
-    config = dotenv_values(".env") 
+    config = dotenv_values(".env")
     if not config:
         config['TOKEN'] = os.getenv('TOKEN')
         config['PORT'] = os.getenv('PORT')
         config['HEROKU_APP_NAME'] = os.getenv('HEROKU_APP_NAME')
 
     application = ApplicationBuilder().token(config['TOKEN']).build()
-    
     start_handler = CommandHandler('start', start)
     voto = CommandHandler('voto', voto)
     application.add_handler(start_handler)
     application.add_handler(voto)
     # application.run_webhook(listen="0.0.0.0",
-                      # port=int(os.environ.get('PORT', 5000)),
-                      # url_path=config['TOKEN'],
-                      # webhook_url=  + config['TOKEN']
+    #                   port=int(os.environ.get('PORT', 5000)),
+    #                   url_path=config['TOKEN'],
+    #                   webhook_url=  + config['TOKEN']
 
     # add handlers
-    application.run_webhook(
-        listen="0.0.0.0",
-        port=int(config['PORT']),
-        url_path=config['TOKEN'],
-        webhook_url=f"https://{config['HEROKU_APP_NAME']}.herokuapp.com/{config['TOKEN']}" 
-    )
+    # application.run_webhook(
+    #    listen="0.0.0.0",
+    #    port=int(config['PORT']),
+    #    url_path=config['TOKEN'],
+    #    webhook_url=f"https://{config['HEROKU_APP_NAME']}.herokuapp.com/{config['TOKEN']}"
+    # )
     # )
     # updater.start_webhook(
-        # listen="0.0.0.0",
-                      # port=int(os.environ.get('PORT', 5000)),
-                      # url_path=telegram_bot_token,
-                      # webhook_url=  + telegram_bot_token
-                      # )
+    #     listen="0.0.0.0",
+    #                   port=int(os.environ.get('PORT', 5000)),
+    #                   url_path=telegram_bot_token,
+    #                   webhook_url=  + telegram_bot_token
+    #                   )
     application.run_polling()
